b0b7553c71ddebfe703e0e5ebf6b2bcd09b4832c
Lancelot OWCZARCZAK <lowczarc@student.42.fr>
2019-04-25 17:17:01 +0200

    array to buffer

--- native/src/lib.rs
+++ native/src/lib.rs
@@ -4,27 +4,31 @@ extern crate beautiful_qrcode_generator_beemly;
 extern crate image;
 
 use std::io::Cursor;
+use std::ops::DerefMut;
 
 use image::{ImageOutputFormat, DynamicImage};
 
 use beautiful_qrcode_generator_beemly::qrcode_generate;
 use neon::prelude::*;
 
-fn generate_qrcode(mut cx: FunctionContext) -> JsResult<JsArrayBuffer> {
+fn generate_qrcode(mut cx: FunctionContext) -> JsResult<JsBuffer> {
     let mut buff = Cursor::new(Vec::new());
     let message = cx.argument::<JsString>(0)?.value();
 
     DynamicImage::ImageRgb8(qrcode_generate(&message)).write_to(&mut buff, ImageOutputFormat::PNG).unwrap();
 
     let vec = buff.into_inner();
-    let js_array = JsArrayBuffer::new(&mut cx, vec.len() as u32)?;
+    let mut js_buffer = JsBuffer::new(&mut cx, vec.len() as u32)?;
 
-    for (i, obj) in vec.iter().enumerate() {
-        let num = cx.number(*obj as f64);
-        js_array.set(&mut cx, i as u32, num)?;
-    }
+    cx.borrow_mut(&mut js_buffer, |mut slice| {
+        let raw = slice.deref_mut().as_mut_slice();
+        for (i, obj) in vec.iter().enumerate() {
+            raw[i] = *obj;
+        }
+    });
+    
 
-    Ok(js_array)
+    Ok(js_buffer)
 }
 
 register_module!(mut cx, {
